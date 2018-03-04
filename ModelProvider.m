f91b3b260306b79de8cd124dab7c8487e5d57722
Junyuan Hong <jyhong836@gmail.com>
2018-03-04 10:23:21 +0800

    Update some comments.

--- ModelProvider.m
+++ ModelProvider.m
@@ -4,16 +4,20 @@ classdef ModelProvider
 methods (Static)
 
 function modelNames = getModelNames ()
-	modelNames =  {'svm_rbf',
+	modelNames =  {'svm_rbf', % SVM classifier with RBF kernel
 	}; 
 end
 
 function [preprocessor, classifier, modelParam] = getModelByName ( name, options )
+% A method provides real models by name.
+% INPUT:
+%   name - The name of model.
+%   options - The option to be provided to `ModelParamProvider`, see `ModelParamProvider.m` for details.
 
 	% Prepare
 	switch name
 		case 'svm_rbf'
-			modelParam   = ModelParamProvider.dg_gau(options);
+			modelParam   = ModelParamProvider.svm_rbf(options);
 			classifier   = @ClassifierProvider.svm;
 			preprocessor = @(data)PreprocessorProvider.kernel_preprocessor(data, 'rbf');
 		otherwise
